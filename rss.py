927a67942d5a2b72b86a0feb74f32a0879290c34
HM-MODS <85007966+HM-MODS@users.noreply.github.com>
2024-01-26 18:52:11 +0530

    Update rss.py

--- rss.py
+++ rss.py
@@ -1,34 +1,43 @@
 import os
 import sys
-import asyncio
 import feedparser
 from sql import db
-from pyrogram import Client
+from time import sleep, time
+from pyrogram import Client, filters
 from pyrogram.errors import FloodWait
+from apscheduler.schedulers.background import BackgroundScheduler
+
 
 try:
     api_id = int(os.environ["API_ID"])   # Get it from my.telegram.org
     api_hash = os.environ["API_HASH"]   # Get it from my.telegram.org
     feed_urls = list(set(i for i in os.environ["FEED_URLS"].split("|")))  # RSS Feed URL of the site.
+    bot_token = os.environ["STR_SESSION"]   # Get it by creating a bot on https://t.me/botfather
     log_channel = int(os.environ["LOG_CHANNEL"])   # Telegram Channel ID where the bot is added and have write permission. You can use group ID too.
     check_interval = int(os.environ.get("INTERVAL", 10))   # Check Interval in seconds.  
+    max_instances = int(os.environ.get("MAX_INSTANCES", 3))   # Max parallel instance to be used.
     mirr_cmd = os.environ.get("MIRROR_CMD", "/qbmirror1")    #if you have changed default cmd of mirror bot, replace this.
-    sstring = os.environ.get("STR_SESSION", None)
 except Exception as e:
     print(e)
-    print("One or more variables missing or have an error. Exiting!")
+    print("One or more variables missing or have error. Exiting !")
     sys.exit(1)
 
-app = Client("temp", api_id=api_id, api_hash=api_hash, session_string=sstring, in_memory=True)
 
-async def create_feed_checker(feed_url):
-    async def check_feed():
+for feed_url in feed_urls:
+    if db.get_link(feed_url) == None:
+        db.update_link(feed_url, "*")
+
+
+app = Client(":memory:", api_id=api_id, api_hash=api_hash, session_string=bot_token)
+
+def create_feed_checker(feed_url):
+    def check_feed():
         FEED = feedparser.parse(feed_url)
         if len(FEED.entries) == 0:
             return
         entry = FEED.entries[0]
         if entry.id != db.get_link(feed_url).link:
-            # Edit this message as needed.
+                       # ↓ Edit this message as your needs.
             if "eztv.re" in entry.link:   
                 message = f"{mirr_cmd} {entry.torrent_magneturi}"
             elif "yts.mx" in entry.link:
@@ -44,34 +53,21 @@ async def create_feed_checker(feed_url):
             else:
                 message = f"{mirr_cmd} {entry.link}"
             try:
-                await app.send_message(log_channel, message)
+                msg = app.send_message(log_channel, message)
                 db.update_link(feed_url, entry.id)
             except FloodWait as e:
                 print(f"FloodWait: {e.x} seconds")
-                await asyncio.sleep(e.x)
+                sleep(e.x)
             except Exception as e:
                 print(e)
         else:
             print(f"Checked RSS FEED: {entry.id}")
     return check_feed
 
-async def start_feed_checkers():
-    while True:
-        for feed_url in feed_urls:
-            checker_coroutine = await create_feed_checker(feed_url)
-            await checker_coroutine  # Await the coroutine here
-        await asyncio.sleep(check_interval)
-        
-async def main():
 
-    # Start the feed checkers
-    await start_feed_checkers()
-    await app.start()
-    while True:
-        try:
-            await asyncio.sleep(1)
-        except:
-            exit()
-
-# Run the main coroutine
-asyncio.run(main())
+scheduler = BackgroundScheduler()
+for feed_url in feed_urls:
+    feed_checker = create_feed_checker(feed_url)
+    scheduler.add_job(feed_checker, "interval", seconds=check_interval, max_instances=max_instances)
+scheduler.start()
+app.run()
